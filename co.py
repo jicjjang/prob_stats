fb6cb2ec37e70eae7a77a2bdc8d60b6324fdfcd6
jyheo <jyheo0@gmail.com>
2013-11-24 18:58:36 +0900

    covariance test

---
+++ co.py
@@ -0,0 +1,12 @@
+import thinkstats.correlation as co
+
+print 'Covariance Test:'
+
+xs = [1, 3, 5, 6, 7]
+ys = [10, 30, 60, 70, 80]
+zs = [7, 6, 5, 3, 1]
+ns = [3, 2, 1, 2, 3]
+
+print co.Cov(xs, ys)
+print co.Cov(xs, zs)
+print co.Cov(xs, ns)
